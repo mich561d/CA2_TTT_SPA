215984a4543696ea56d787dcd08cef1d505d9bdc
Jesper Christensen <jesper300595@gmail.com>
2019-03-13 23:24:50 +0100

    Tried to create option/select and inputs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = document.getElementById("root");
+var what = document.getElementById("select");
+var how = document.getElementById("how");
+
+alert(what.value);
 
 root.innerHTML = "Hello World";
+
+// function createSelect() {
+//   alert(what.value);
+//   if (what.value == "company") {
+//     var test = new Option("test", "this");
+//     selbox.options[selbox.options.length] = test;
+//     test.setAttribute("test", "that");
+//   }
+// }
+
+// createSelect();
