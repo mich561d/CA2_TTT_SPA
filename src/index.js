fca3e27c8085af6fd367fa2e2a494401ccd05e53
Jesper Christensen <jesper300595@gmail.com>
2019-03-17 03:16:59 +0100

    CRUD Methods added, Functionality with inputs still failing

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ var how = document.getElementById("how");
 var input = document.getElementById("input");
 var fetchButton = document.getElementById("fetchButton");
 var dataDiv = document.getElementById("data");
+var clearData = document.getElementById("clearData");
 
 what.innerHTML = choicesToHTML(whatChoices);
 what.addEventListener("change", addChoicesToHow);
@@ -21,6 +22,11 @@ how.innerHTML = choicesToHTML(companyChoices);
 how.addEventListener("change", setInputField);
 setInputField();
 fetchButton.addEventListener("click", fetchData);
+clearData.addEventListener("click", clearData);
+
+function clearData() {
+    dataDiv.innerHTML = "<h1>Data has been cleared</h1>";
+}
 
 function addChoicesToHow() {
     var selectedWhat = what.value;
@@ -131,11 +137,169 @@ function personToHTML(json) {
 }
 
 function hobbyToHTML(json) {
-    var html = json;
-    return html;
+    var html = json.map(hobby => "<li>Hobby: " + hobby.name + " - Description: " + hobby.description + "</li>").join("");
+    return "<ul>" + html + "</ul>";
 }
 
 function cityToHTML(json) {
     var html = json.map(city => "<li>City: " + city.city + " - Zip: " + city.zipCode + "</li>").join("");
     return "<ul>" + html + "</ul>";
+}
+
+//**************CRUD ************************************* */
+
+const crud =  ["Create", "Update"]
+const whatCRUD = ["Company", "Person", "Hobby"];
+var choice = document.getElementById("selectCRUD");
+var what1 = document.getElementById("createSel");
+var input2 = document.getElementById("input2")
+var btnCU = document.getElementById("btnCU");
+
+choice.innerHTML = choicesToHTML(crud);
+what1.innerHTML = choicesToHTML(whatCRUD);
+what1.addEventListener("change", createInputFields);
+setInputField2();
+btnCU = addEventListener("click",postMethod);
+
+function setInputField2() {
+    input2.innerHTML = createInputFields();
+}
+
+//Den er stuck med Company inputsene.. 
+function createInputFields() {
+    var selectedCRUD = what1.value;
+    console.log(selectedCRUD);
+    var inputs = "";
+    switch (selectedCRUD) {   
+        case "Company":
+            inputs += "<input id=\"cID\" class=\"form-control\" type=\"number\" placeholder=\"ID (Only for update)\">";
+            inputs += "<input id=\"cName\" class=\"form-control\" type=\"text\" placeholder=\"Company name..\">";
+            inputs += "<input id=\"cDesc\" class=\"form-control\" type=\"text\" placeholder=\"Company description..\">";
+            inputs += "<input id=\"cCVR\" class=\"form-control\" type=\"number\" value=\"70000601\">";
+            inputs += "<input id=\"cNumOfEmp\" class=\"form-control\" type=\"number\" placeholder=\"Amount of employees..\">";
+            inputs += "<input id=\"cMarketValue\" class=\"form-control\" type=\"number\" placeholder=\"Market value..\">";
+            inputs += "<input id=\"cEmail\" class=\"form-control\" type=\"text\" placeholder=\"Email\">";
+            inputs += "<input id=\"cPhone\" class=\"form-control\" type=\"number\" placeholder=\"Phone number\">";
+            inputs += "<input id=\"cAddress\" class=\"form-control\" type=\"text\" placeholder=\"Address\">";
+            return inputs;
+        case "Person":
+            inputs += "<input id=\"pID\" type=\"number\" placeholder=\"ID (Only for update)\">";
+            inputs += "P<input id=\"pFName\" type=\"text\"> <br>";
+            inputs += "<input id=\"pLName\" type=\"text\"> <br>";
+            inputs += "<input id=\"pHobby\" type=\"text\"> <br>";
+            inputs += "<input id=\"pEmail\" type=\"text\"> <br>";
+            inputs += "<input id=\"pPhone\" type=\"number\"> <br>";
+            inputs += "<input id=\"pAdress\" type=\"text\"> <br>";
+            return inputs;
+        case "Hobby":
+            inputs += "<input id=\"hID\" type=\"number\" placeholder=\"ID (Only for update)\">";             
+            inputs += "H<input id=\"hName\" type=\"text\" placeholder=\"Write the name here..\">";
+            inputs += "<input id=\"hDesc\" type=\"text\"> placeholder=\"Write the desc here..\"";
+            return inputs;
+        default:
+            alert("Something went wrong, please try again or reload page (F5)");
+            return "<input type=\"text\" disabled=\"disabled\">";
+    }
+}
+
+function postMethod() {
+    var data = "";
+    var alertMessage = "";
+    var method = '';
+    const chosenValue = what1.value;
+    if(choice.value === "Create") {
+        data = createData;
+        method = "POST"
+        var alertMessage = "Entity has been created"
+    } else {
+        data = updateData;
+        method = 'PUT'
+        var alertMessage = "Entity has been updated"
+    }
+    fetch(url.concat(chosenValue), {
+        method: method,
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).then((res) => res.json()).then((data) => {
+        console.log('data from post', data);        
+        alertMessage(alertMessage);
+    }).catch(error => console.log(error));
+}
+
+function createData() {
+    var selectedEntity = what1.value;
+    switch(selectedEntity) {
+        case "Hobby":
+            var hName = document.getElementById("hName");
+            var hDesc = document.getElementById("hDesc");
+            var hData = {hName, hDesc}
+            return hData;
+        case "Company":
+            var cName = document.getElementById("cName");
+            var cDesc = document.getElementById("cDesc");
+            var cCVR = document.getElementById("cCVR");
+            var cNumOfEmp = document.getElementById("cNumOfEmp");
+            var cMarketValue = document.getElementById("cMarketValue");
+            var cEmail = document.getElementById("cEmail");
+            var cPhone = document.getElementById("cPhone");
+            var cAddress = document.getElementById("cAddress");
+            var cData = {cName, cDesc, cCVR, cNumOfEmp, cMarketValue, cEmail, cPhone, cAddress};
+            return cData;
+        case "Person":
+            var pFName = document.getElementById("pFName");
+            var pLName = document.getElementById("pLName");
+            var pHobby = document.getElementById("pHobby");
+            var pEmail = document.getElementById("pEmail");
+            var pPhone = document.getElementById("pPhone");
+            var pAddress = document.getElementById("pAddress");
+            var pData = {pFName, pLName, pHobby, pEmail, pPhone, pAddress}
+            return pData;
+    }
+}
+
+function updateData() {
+    var selectedEntity = what1.value;
+    switch(selectedEntity) {
+        case "Hobby":
+            var hId;
+            var hName = document.getElementById("hName");
+            var hDesc = document.getElementById("hDesc");
+            var hData = {hId, hName, hDesc}
+            return hData;
+        case "Company":
+            var cId;
+            var cName = document.getElementById("cName");
+            var cDesc = document.getElementById("cDesc");
+            var cCVR = document.getElementById("cCVR");
+            var cNumOfEmp = document.getElementById("cNumOfEmp");
+            var cMarketValue = document.getElementById("cMarketValue");
+            var cEmail = document.getElementById("cEmail");
+            var cPhone = document.getElementById("cPhone");
+            var cAddress = document.getElementById("cAddress");
+            var cData = {cName, cDesc, cCVR, cNumOfEmp, cMarketValue, cEmail, cPhone, cAddress};
+            return cData;
+        case "Person":
+            var pId;
+            var pFName = document.getElementById("pFName");
+            var pLName = document.getElementById("pLName");
+            var pHobby = document.getElementById("pHobby");
+            var pEmail = document.getElementById("pEmail");
+            var pPhone = document.getElementById("pPhone");
+            var pAddress = document.getElementById("pAddress");
+            var pData = {pFName, pLName, pHobby, pEmail, pPhone, pAddress}
+            return pData;
+    }
+}//************DELETE ********************************/
+var selectDelete = document.getElementById("selectDelete");
+var deleteID = document.getElementById("deleteID");
+var urlDelete = ""
+selectDelete.innerHTML = choicesToHTML(whatCRUD);
+
+
+function deleteEntity() {
+    const selectedID = deleteID.value;
+    const entityToDelete = selectDelete.value; 
+    fetch(url + entityToDelete + "/delete/id=" + selectedID, { method: 'DELETE' }).then(res => res.json()).then((data) => {
+        getAllPersons();
+    }).catch(error => console.log(error));
 }
